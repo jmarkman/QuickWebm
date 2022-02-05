908c675bd78f2bac23cdc51b24e4bb34688c63f0
Jonathan Markman <jmarkman446@gmail.com>
2022-02-05 01:07:19 -0500

    Inject dialog service into viewmodels to display popup dialogs to users

--- QuickWebm/App.xaml.cs
+++ QuickWebm/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuickWebm.Services;
+using QuickWebm.ViewModels;
+using QuickWebm.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,36 @@ namespace QuickWebm
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceContainer { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ServiceContainer = ConfigureServices();
+
+            var window = ServiceContainer.GetRequiredService<MainWindow>();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton<QuickWebmConverterViewModel>();
+            serviceCollection.AddSingleton(svc => new MainWindow()
+            {
+                DataContext = svc.GetRequiredService<QuickWebmConverterViewModel>()
+            });
+            serviceCollection.AddSingleton(svc => new VideoEffectsView()
+            {
+                DataContext = svc.GetRequiredService<VideoEffectsViewModel>()
+            });
+            serviceCollection.AddSingleton<WebmEncodingViewModel>();
+            serviceCollection.AddSingleton<AdvancedOptionsViewModel>();
+            serviceCollection.AddSingleton<IDialogService, DialogService>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
     }
 }
