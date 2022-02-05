1e691794ed5296399204764bce3ca422f27aaa93
Jonathan Markman <jmarkman446@gmail.com>
2022-02-04 21:06:20 -0500

    Create dialog service for opening dialog windows in the application

---
+++ QuickWebm/Services/DialogService.cs
@@ -0,0 +1,26 @@
+﻿using QuickWebm.ViewModels;
+using QuickWebm.Views;
+using System;
+using System.Collections.Generic;
+
+namespace QuickWebm.Services
+{
+    public class DialogService : IDialogService
+    {
+        static readonly Dictionary<Type, Type> dialogViewModelMap = new()
+        {
+            { typeof(VideoResolutionViewModel), typeof(VideoResolutionView) }
+        };
+
+        public void ShowDialog<TViewModel>()
+        {
+            var viewModelType = dialogViewModelMap[typeof(TViewModel)];
+            var dialog = new DialogWindow
+            {
+                Content = Activator.CreateInstance(viewModelType)
+            };
+
+            dialog.ShowDialog();
+        }
+    }
+}
